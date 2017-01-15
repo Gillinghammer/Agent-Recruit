b7a0219a43de847e347a55ad546db4cd4662ce2c
Gillinghammer <colingillingham@gmail.com>
2017-01-15 15:37:55 +0000

    hid db credentials in env vars

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ https.createServer({
       cert: fs.readFileSync('cert.pem')
     }, app).listen(3000);
 
-mongoose.connect('mongodb://admin:admin@ds011382.mlab.com:11382/anaescape');
+mongoose.connect("mongodb://" + process.env.AGENT_DB_USER +":" + process.env.AGENT_DB_PASS + "@ds011382.mlab.com:11382/anaescape");
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
